914d3e5ad27e5566e66b325d2b97ad7b6e71c7a9
vishwas <vishwas@DESKTOP-36JED47>
2019-11-01 16:01:33 +0530

    design

---
+++ src/Bigbang/galaxy.java
@@ -0,0 +1,11 @@
+package Bigbang;
+
+public class galaxy {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+       System.out.println("twinkle twinkle litle star");
+	}
+
+}
